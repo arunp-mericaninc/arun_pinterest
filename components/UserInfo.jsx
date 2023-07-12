01a3b98dd19e4d9b013a8766320b40bd43d811e9
arunp-mericaninc <arunp@mericaninc.com>
2023-07-12 14:53:58 +0530

    added path

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
+import Pin_img from "./Pin-docs/Pin_img";
 
 const UserInfo = ({ session }) => {
     const router= useRouter()
