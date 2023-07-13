14f7a0a1bf618b5468723148216945eab175d28d
arunp-mericaninc <arunp@mericaninc.com>
2023-07-13 11:44:18 +0530

    Added Image Property

--- components/Pin-docs/Pin_img.jsx
+++ components/Pin-docs/Pin_img.jsx
@@ -1,28 +1,36 @@
-'use client'
-import Image from 'next/image'
-import React from 'react'
-import UserTag from '../UserTag';
-import UserImage from '../UserImage';
+"use client";
+import Image from "next/image";
+import React from "react";
+import UserTag from "../UserTag";
+import UserImage from "../UserImage";
 
-const Pin_img = ({post,session}) => {
-    console.log(post);
-    const handlePost=()=>{
-
-    }
-    console.log(post);
+const Pin_img = ({ post, session }) => {
+  console.log(post);
+  const handlePost = () => {};
+  console.log(post);
   return (
-    <div className='columns-1 md:columns-2 lg:columns-4 xl:columns-5  space-y-6 m-4 ' >
-        {post.map((item)=>{
-            return(
-                <div key={item.id} onClick={()=>handlePost(item.id)} className='flex flex-col hover:bg-slate-100 rounded-3xl items-center gap-1'>
-                    <Image className='shadow-md  rounded-3xl' src={item.image} alt={item.title} width={250} height={100}/>
-                    <p className='text-center font-bold'>{item.title}</p>
-                    {session?<UserTag user={session?.user} />:<UserImage user={item}/>}
-                </div>
-            )
-        })}
+    <div className="columns-1 md:columns-2 lg:columns-4 xl:columns-5 space-y-6 m-2 ">
+      {post.map((item) => {
+        return (
+          <div
+            key={item.id}
+            onClick={() => handlePost(item.id)}
+            className="flex flex-col relative max-w-xs overflow-hidden hover:opacity-110 bg-cover bg-no-repeat rounded-3xl items-center gap-1"
+          >
+            <Image
+              className="shadow-md  rounded-3xl transition duration-300 ease-in-out hover:scale-110 "
+              src={item.image}
+              alt={item.title}
+              width={250}
+              height={100}
+            />
+            {/* <p className='text-center font-bold'>{item.title}</p>
+                    {session?<UserTag user={session?.user} />:<UserImage user={item}/>} */}
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Pin_img
+export default Pin_img;
